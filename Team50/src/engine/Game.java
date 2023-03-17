93d0bc1f40ee861da0867183442ccd9d7cbc27fe
Ehab <Ehab@192.168.1.23>
2023-03-17 09:47:30 +0200

    trial commit

---
+++ Team50/src/engine/Game.java
@@ -0,0 +1,13 @@
+package engine;
+import java.util.ArrayList;
+
+public class Game {
+	public static ArrayList<Hero> availableHeros;
+	public static ArrayList<Hero> heros;
+	public static ArrayList<Zombie> zombies;
+	public static Cell [][] map;
+	
+	public static void loadHeros(String filePath) {
+		
+	}
+}
